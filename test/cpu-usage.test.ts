599515a94d6a43fac203394244b956d71eda4b53
Sascha Tandel <s.tandel@kenoxa.de>
2020-10-21 14:17:59 +0200

    chore

--- test/cpu-usage.test.ts
+++ test/cpu-usage.test.ts
@@ -28,19 +28,19 @@ test('processCpuUsage', async () => {
     '# HELP process_cpu_user_seconds_total Total user CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE process_cpu_user_seconds_total counter')
-  expect(metrics).toMatch(/^process_cpu_user_seconds_total \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^process_cpu_user_seconds_total \d+(?:\.\d+)? \d{13}$/m)
 
   expect(metrics).toMatch(
     '# HELP process_cpu_system_seconds_total Total system CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE process_cpu_system_seconds_total counter')
-  expect(metrics).toMatch(/^process_cpu_system_seconds_total \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^process_cpu_system_seconds_total \d+(?:\.\d+)? \d{13}$/m)
 
   expect(metrics).toMatch(
     '# HELP process_cpu_seconds_total Total user and system CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE process_cpu_seconds_total counter')
-  expect(metrics).toMatch(/^process_cpu_seconds_total \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^process_cpu_seconds_total \d+(?:\.\d+)? \d{13}$/m)
 })
 
 test('processCpuUsage (with labels)', async () => {
@@ -54,19 +54,19 @@ test('processCpuUsage (with labels)', async () => {
     '# HELP process_cpu_user_seconds_total Total user CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE process_cpu_user_seconds_total counter')
-  expect(metrics).toMatch(/^process_cpu_user_seconds_total\{one="two"} \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^process_cpu_user_seconds_total\{one="two"} \d+(?:\.\d+)? \d{13}$/m)
 
   expect(metrics).toMatch(
     '# HELP process_cpu_system_seconds_total Total system CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE process_cpu_system_seconds_total counter')
-  expect(metrics).toMatch(/^process_cpu_system_seconds_total\{one="two"} \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^process_cpu_system_seconds_total\{one="two"} \d+(?:\.\d+)? \d{13}$/m)
 
   expect(metrics).toMatch(
     '# HELP process_cpu_seconds_total Total user and system CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE process_cpu_seconds_total counter')
-  expect(metrics).toMatch(/^process_cpu_seconds_total\{one="two"} \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^process_cpu_seconds_total\{one="two"} \d+(?:\.\d+)? \d{13}$/m)
 })
 
 test('processCpuUsage (custom prefix)', async () => {
@@ -80,19 +80,19 @@ test('processCpuUsage (custom prefix)', async () => {
     '# HELP nodejs_cpu_user_seconds_total Total user CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE nodejs_cpu_user_seconds_total counter')
-  expect(metrics).toMatch(/^nodejs_cpu_user_seconds_total \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^nodejs_cpu_user_seconds_total \d+(?:\.\d+)? \d{13}$/m)
 
   expect(metrics).toMatch(
     '# HELP nodejs_cpu_system_seconds_total Total system CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE nodejs_cpu_system_seconds_total counter')
-  expect(metrics).toMatch(/^nodejs_cpu_system_seconds_total \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^nodejs_cpu_system_seconds_total \d+(?:\.\d+)? \d{13}$/m)
 
   expect(metrics).toMatch(
     '# HELP nodejs_cpu_seconds_total Total user and system CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE nodejs_cpu_seconds_total counter')
-  expect(metrics).toMatch(/^nodejs_cpu_seconds_total \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^nodejs_cpu_seconds_total \d+(?:\.\d+)? \d{13}$/m)
 })
 
 test('processCpuUsage (custom name)', async () => {
@@ -106,17 +106,17 @@ test('processCpuUsage (custom name)', async () => {
     '# HELP process_core_user_seconds_total Total user CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE process_core_user_seconds_total counter')
-  expect(metrics).toMatch(/^process_core_user_seconds_total \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^process_core_user_seconds_total \d+(?:\.\d+)? \d{13}$/m)
 
   expect(metrics).toMatch(
     '# HELP process_core_system_seconds_total Total system CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE process_core_system_seconds_total counter')
-  expect(metrics).toMatch(/^process_core_system_seconds_total \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^process_core_system_seconds_total \d+(?:\.\d+)? \d{13}$/m)
 
   expect(metrics).toMatch(
     '# HELP process_core_seconds_total Total user and system CPU time spent in seconds.',
   )
   expect(metrics).toMatch('# TYPE process_core_seconds_total counter')
-  expect(metrics).toMatch(/^process_core_seconds_total \d+\.\d+ \d{13}$/m)
+  expect(metrics).toMatch(/^process_core_seconds_total \d+(?:\.\d+)? \d{13}$/m)
 })
